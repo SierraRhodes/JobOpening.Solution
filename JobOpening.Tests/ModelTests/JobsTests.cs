2300c0880ff88f4a68ddec4f7787e5e8dbefc1b6
SierraRhodes <alreadywrote@gmail.com>
2023-04-24 19:28:06 -0700

    add job instance test passed

---
+++ JobOpening.Tests/ModelTests/JobsTests.cs
@@ -0,0 +1,20 @@
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using JobOpening.Models;
+
+namespace JobOpening.Tests
+{
+  [TestClass]
+  public class JobTests
+  {
+   [TestMethod]
+    public void Job_InstantiateJob_ReturnJob()
+    {
+      Job testJob = new Job("Software Engineer", "Fixing Bugs", "Email");
+
+      Assert.AreEqual("Software Engineer", testJob.Title);
+      Assert.AreEqual("Fixing Bugs", testJob.Description);
+      Assert.AreEqual("Email", testJob.ContactInfo);
+    }
+   
+  }
+}
